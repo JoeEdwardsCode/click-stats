a4fd31e5fad2950eca2e9d827145dbaac1b6dfc4
Joe <joe@joedevelops.com>
2022-07-24 19:03:11 -0700

    Adding module for a generic response

---
+++ utils/response.go
@@ -0,0 +1,25 @@
+package response
+
+import (
+	"bytes"
+	"encoding/json"
+
+	"github.com/aws/aws-lambda-go/events"
+)
+
+type Response events.APIGatewayProxyResponse
+
+func ClickStatsResponse(statusCode int, body []byte) Response {
+
+	var buf bytes.Buffer
+	json.HTMLEscape(&buf, body)
+
+	return Response{
+		StatusCode:      statusCode,
+		IsBase64Encoded: false,
+		Body:            buf.String(),
+		Headers: map[string]string{
+			"Content-Type": "application/json",
+		},
+	}
+}
