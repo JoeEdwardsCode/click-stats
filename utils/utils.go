b190aed995e4f561ce052f3403eec89df717c950
Joe <joe@joedevelops.com>
2022-07-24 19:42:11 -0700

    Updating package contents to not export response type

--- utils/utils.go
+++ utils/utils.go
@@ -7,14 +7,12 @@ import (
 	"github.com/aws/aws-lambda-go/events"
 )
 
-type Response events.APIGatewayProxyResponse
-
-func ClickStatsResponse(statusCode int, body []byte) Response {
+func ClickStatsResponse(statusCode int, body []byte) events.APIGatewayProxyResponse {
 
 	var buf bytes.Buffer
 	json.HTMLEscape(&buf, body)
 
-	return Response{
+	return events.APIGatewayProxyResponse{
 		StatusCode:      statusCode,
 		IsBase64Encoded: false,
 		Body:            buf.String(),
