f28d6c44fb32df059a885547c76fab2211719be3
Joe <joe@joedevelops.com>
2022-07-28 10:52:05 -0700

    Made the util a little more flexible with how it writes errors v status messages

--- src/utils/main.go
+++ src/utils/main.go
@@ -8,14 +8,20 @@ import (
 )
 
 func ClickStatsResponse(statusCode int, body []byte) events.APIGatewayProxyResponse {
+	var key = "data"
 
-	var buf bytes.Buffer
-	json.HTMLEscape(&buf, body)
+	if statusCode != 200 {
+		key = "error"
+	}
+
+	jsonResponse, _ := json.Marshal(map[string]string{key: string(body)})
+	var buffer bytes.Buffer
+	json.HTMLEscape(&buffer, jsonResponse)
 
 	return events.APIGatewayProxyResponse{
 		StatusCode:      statusCode,
 		IsBase64Encoded: false,
-		Body:            buf.String(),
+		Body:            buffer.String(),
 		Headers: map[string]string{
 			"Content-Type": "application/json",
 		},
