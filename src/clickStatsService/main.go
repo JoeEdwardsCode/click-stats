f53a4e623b620067c2fa5962b1b1086203b87ec3
Joe <joe@joedevelops.com>
2022-07-28 10:53:27 -0700

    Completing DB write with use of environment variables.  Added logging of input

--- src/clickStatsService/main.go
+++ src/clickStatsService/main.go
@@ -4,10 +4,14 @@ import (
 	"context"
 	"encoding/json"
 	"errors"
+	"fmt"
+	"os"
+	"time"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
 )
 
 type ClickEvent struct {
@@ -16,7 +20,12 @@ type ClickEvent struct {
 	Quadrant string `json:"quadrant"`
 }
 
-type ClickRecord struct {
+type ClickEventTableRecord struct {
+	record_type string
+	timestamp   string
+	shape       string
+	color       string
+	quadrant    string
 }
 
 func getClickEvent(eventBody []byte) (ClickEvent, error) {
@@ -30,34 +39,47 @@ func getClickEvent(eventBody []byte) (ClickEvent, error) {
 	return clickEvent, nil
 }
 
-func recordClick(clickEvent ClickEvent) (*dynamodb.PutItemOutput, error) {
-	var tableName string = "clickEvents"
-	input := &dynamodb.PutItemInput{
-		Item:      nil,
+func RecordClickEvent(event events.APIGatewayProxyRequest) error {
+	clickEvent, eventError := getClickEvent([]byte(event.Body))
+
+	if eventError != nil {
+		return eventError
+	}
+
+	var tableName string = os.Getenv("TABLE_NAME")
+	tableRecord := ClickEventTableRecord{
+		record_type: "CLICK",
+		timestamp:   time.Now().UTC().String(),
+		color:       clickEvent.Color,
+		shape:       clickEvent.Shape,
+		quadrant:    clickEvent.Quadrant,
+	}
+
+	itemMap := map[string]types.AttributeValue{
+		"record_type": &types.AttributeValueMemberS{Value: tableRecord.record_type},
+		"timestamp":   &types.AttributeValueMemberS{Value: tableRecord.timestamp},
+		"shape":       &types.AttributeValueMemberS{Value: tableRecord.shape},
+		"quadrant":    &types.AttributeValueMemberS{Value: tableRecord.quadrant},
+		"color":       &types.AttributeValueMemberS{Value: tableRecord.color},
+	}
+
+	itemInput := &dynamodb.PutItemInput{
+		Item:      itemMap,
 		TableName: &tableName,
 	}
 
 	config, configError := config.LoadDefaultConfig(context.TODO())
 	if configError != nil {
-		return nil, configError
+		return configError
 	}
 
+	fmt.Printf("Writing item:\n%+v", tableRecord)
 	client := dynamodb.NewFromConfig(config)
+	_, putError := client.PutItem(context.TODO(), itemInput)
 
-	return client.PutItem(context.TODO(), input)
-}
+	if putError != nil {
+		return putError
+	}
 
-func RecordClickEvent(context context.Context, event events.APIGatewayProxyRequest) ([]byte, error) {
-	// var applicationError error
-	// eventBody := []byte(event.Body)
-
-	// clickEvent, applicationError := getClickEvent(eventBody)
-
-	// if applicationError != nil {
-	// 	return nil, applicationError
-	// }
-
-	// recordClickEventResponse, applicationError := recordClick(clickEvent)
-
-	return nil, nil
+	return nil
 }
